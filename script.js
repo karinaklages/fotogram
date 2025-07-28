4a0b73077f7579c3a22046d1436df62a5385ffac
Karina Klages <karinaklages@web.de>
2025-07-28 10:05:10 +0200

    new function and css added

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 
 
+
 let imagesArray = [
 "./img/clownfish-3030148.jpg",
 "./img/coral-2694453.jpg",
@@ -15,6 +16,41 @@ let imagesArray = [
 "./img/reef-tank-3624204.jpg",
 "./img/penguin-2203693.jpg",
 ];
+
+
+
+let imageNamesArray = [
+"Clownfish",
+"Coral",
+"Fighting Fish",
+"Colorful Fish",
+"Turtle",
+"Fighting Fish",
+"Coral",
+"Octopus",
+"Jellyfish",
+"Colorful Fish",
+"Reef Tank",
+"Penguin",
+];
+
+
+
+let countNumbersArray = [
+"1 / 12",
+"2 / 12",
+"3 / 12",
+"4 / 12",
+"5 / 12",
+"6 / 12",
+"7 / 12",
+"8 / 12",
+"9 / 12",
+"10 / 12",
+"11 / 12",
+"12 / 12",
+];
+
 
 
 // Renders the images out of the array
@@ -34,6 +70,25 @@ function render() {
 }
 
 render()
+// Funktioniert mit onclick="openDialog()" in index.html Zeile 23. Allerdings wird dann die ganze div beim onclick-Event angewählt, nicht das einzelne Foto. 
+
+
+
+/*
+// Renders the images out of the array
+function render() {
+
+    let placeImages = document.getElementById("displayImageContainer")
+    placeImages.innerHTML = ""; 
+
+    for (let index = 0; index < imagesArray.length; index++) {
+        placeImages.innerHTML += 
+    }
+}
+
+render() 
+*/
+
 
 
 // Opens and closes the image dialog
