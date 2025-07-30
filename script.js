3ddf966c19e04a61d645becbdcbc149a4b1e1b76
Karina Klages <karinaklages@web.de>
2025-07-30 14:56:51 +0200

    script.js with functions updated, mistakes with event-bubbling updated

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
-"use strict";
-
-
 let imagesArray = [
-    { url: "./img/clownfish-3030148.jpg", title: "Clownfish", alt: "Clownfish in the ocean", counter: "1 / 12" },
-    { url: "./img/coral-2694453.jpg", title: "Coral", alt: "Coral in the ocean", counter: "2 / 12" },
-    { url: "./img/fighting-fish-2009972.jpg", title: "Fighting Fish", alt: "Fighting fish in the ocean", counter: "3 / 12" },
-    { url: "./img/fish-7383981.jpg", title: "Colorful Fish", alt: "Colorful fish in the ocean", counter: "4 / 12" },
-    { url: "./img/animal-1866808.jpg", title: "Turtle",  alt: "Turtle in the ocean",counter: "5 / 12" },
-    { url: "./img/fighting-fish-2009971.jpg", title: "Fighting Fish", alt: "Fighting fish in the ocean", counter: "6 / 12" },
-    { url: "./img/coral-4749104.jpg", title: "Coral", alt: "Coral in the ocean", counter: "7 / 12" },
-    { url: "./img/octopus-7113989.jpg", title: "Octopus", alt: "Octopus in the ocean", counter: "8 / 12" },
-    { url: "./img/jellyfish-6653502.jpg", title: "Jellyfish", alt: "Jellyfish in the ocean", counter: "9 / 12" },
-    { url: "./img/fish-8250928.jpg", title: "Colorful Fish", alt: "Colorful fish in the ocean", counter: "10 / 12" },
-    { url: "./img/reef-tank-3624204.jpg", title: "Reef Tank", alt: "Reef tank in the ocean",  counter: "11 / 12" },
-    { url: "./img/penguin-2203693.jpg", title: "Penguin",  alt: "Penguin in the ocean", counter: "12 / 12" },
+    { url: "./img/clownfish-3030148.jpg", title: "Clownfish", alt: "Clownfish in the ocean" },
+    { url: "./img/coral-2694453.jpg", title: "Coral", alt: "Coral in the ocean" },
+    { url: "./img/fighting-fish-2009972.jpg", title: "Fighting Fish", alt: "Fighting fish in the ocean" },
+    { url: "./img/fish-7383981.jpg", title: "Colorful Fish", alt: "Colorful fish in the ocean" },
+    { url: "./img/animal-1866808.jpg", title: "Turtle",  alt: "Turtle in the ocean" },
+    { url: "./img/fighting-fish-2009971.jpg", title: "Fighting Fish", alt: "Fighting fish in the ocean" },
+    { url: "./img/coral-4749104.jpg", title: "Coral", alt: "Coral in the ocean" },
+    { url: "./img/octopus-7113989.jpg", title: "Octopus", alt: "Octopus in the ocean" },
+    { url: "./img/jellyfish-6653502.jpg", title: "Jellyfish", alt: "Jellyfish in the ocean" },
+    { url: "./img/fish-8250928.jpg", title: "Colorful Fish", alt: "Colorful fish in the ocean" },
+    { url: "./img/reef-tank-3624204.jpg", title: "Reef Tank", alt: "Reef tank in the ocean" },
+    { url: "./img/penguin-2203693.jpg", title: "Penguin",  alt: "Penguin in the ocean" },
 ];
 
 
 let index = 0;
 let currentImageIndex = 0;
+const dialogRef = document.getElementById("imageDialog");
+const overlayRef = document.getElementById("dialogOverlay");
 
 
 // Renders the images out of the array with onload="render()" in index.html
@@ -33,30 +32,27 @@ function render() {
 }
 
 function imageTemplate(index) {
-    const image = imagesArray[index];
-    return  `<img src="${image.url}" 
-            title="${image.title}" 
-            alt="${image.alt}"
+    return  `<img src="${imagesArray[index].url}" 
+            title="${imagesArray[index].title}" 
+            alt="${imagesArray[index].alt}"
             onclick="openDialog(${index})"  
             class="imageStyle"
             tabindex="0"> 
             `;
 }
 
-render() 
-
 
 // Opens and closes the dialog
-const dialogRef = document.getElementById("imageDialog");
-const overlayRef = document.getElementById("dialogOverlay");
-
 function openDialog(index) {
-    renderDialog(index);
+    currentImageIndex = index;
+    document.body.style.overflow = 'hidden';
+    renderDialog(currentImageIndex);
     overlayRef.style.display = "flex";
 }
 
 function closeDialog() {
     overlayRef.style.display = "none";
+    document.body.style.overflow = 'auto';
 }
 
 
@@ -74,18 +70,17 @@ function renderDialog(index) {
 }
 
 function dialogTemplate(index) {
-    const image = imagesArray[index];
     return `   
         <header>
             <h2 id="dialogHeadline">Underwater World</h2>
             <div class="subheadline">
-                <p id="dialogImageName">${image.title}</p>
-                <p id="countImageNumber">${image.counter}</p>
+                <p id="dialogImageName">${imagesArray[index].title}</p>
+                <p id="countImageNumber">${index + 1} / ${imagesArray.length}</p>
             </div>
         </header>
 
         <div id="dialogImages">
-            <img src="${image.url}" title="${image.title}" alt="${image.alt}" class="showDialogImage">
+            <img src="${imagesArray[index].url}" title="${imagesArray[index].title}" alt="${imagesArray[index].alt}" class="showDialogImage">
         </div>
 
         <footer class="dialogFooter">
@@ -105,8 +100,6 @@ function dialogTemplate(index) {
         </footer>
     `;
 }
-
-renderDialog(index)
 
 
 // Navigates the arrow buttons within the dialog 
